e552a04272ff66d99a761ff3a7a342ca410ec6de
Dashrath1025 <dashrathkanzariya1025@gmail.com>
2023-06-28 21:53:54 +0530

    Add project files.

---
+++ Testing/DataModels/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Testing.Models
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options) { }
+        public DbSet<Student> Student { get; set; } 
+        public DbSet<Gender> Gender { get; set; } 
+        public DbSet<Address> Address { get; set; } 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
